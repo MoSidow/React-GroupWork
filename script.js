9d208f7ba5b9a037ddfb8e6742bcd9da3cdec8e6
Ahmed Alasow <a.alasow01@gmail.com>
2022-11-06 15:23:44 +0000

    Added lookUp function to find weather information from the user input (Early stages so bugs are present)

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+const weatherAPIKey = '272b68b95d1c42ff7655c2f715fa4879';
+const APIURL = 'https://api.openweathermap.org/';
+
+
 var myLocation = document.getElementById('location');
 var searchLocation = document.getElementById('search');
-
-var weatherAPIKey = '272b68b95d1c42ff7655c2f715fa4879';
-var APIURL = 'https://api.openweathermap.org/';
 
 var fiveDayForecast = 5;
 
@@ -11,6 +12,27 @@ function getLocation(){
   var userInput = myLocation.value;
 
   console.log('Pressed');
+
+  lookUp(myLocation);
+}
+
+// Searches the weather for the location the user has entered, using the weather api to access up to date information
+function lookUp(search){
+
+  var queryAPI = `${weatherAPIKey}geo/1.0/direct?q=${search}&limit=5&appid=${APIURL}`
+
+  fetch(queryAPI)
+
+    .then((response) => response.json())
+
+    .then(data => {
+
+      // Sets the locations weather information (lat, lon, humidity, temp) into an array, in case there are other locations with similar names to it.
+      const locationInput = data[0];
+
+      console.log(locationInput)
+    });
+  
 }
 
 searchLocation.addEventListener("click", getLocation);
