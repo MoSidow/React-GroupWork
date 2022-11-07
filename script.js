1837817893591434eda4aaeadf214be5794d90cb
Ahmed Alasow <a.alasow01@gmail.com>
2022-11-07 16:09:09 +0000

    Fixed location display in console bug

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const weatherAPIKey = '272b68b95d1c42ff7655c2f715fa4879';
-const APIURL = 'https://api.openweathermap.org/';
+const APIURL = "https://api.openweathermap.org";
+const APIKEY = "272b68b95d1c42ff7655c2f715fa4879";
 
 
 var myLocation = document.getElementById('location');
@@ -13,25 +13,26 @@ function getLocation(){
 
   console.log('Pressed');
 
-  lookUp(myLocation);
+  // Outputs the result of the function once the user has searched for their desired location
+  lookUp(userInput);
 }
 
 // Searches the weather for the location the user has entered, using the weather api to access up to date information
 function lookUp(search){
 
-  var queryAPI = `${weatherAPIKey}geo/1.0/direct?q=${search}&limit=5&appid=${APIURL}`
+  var queryAPI = `${APIURL}/geo/1.0/direct?q=${search}&limit=5&appid=${APIKEY}`;
 
   fetch(queryAPI)
-
     .then((response) => response.json())
 
     .then(data => {
 
-      // Sets the locations weather information (lat, lon, humidity, temp) into an array, in case there are other locations with similar names to it.
+      // Sets the locations weather information (lat, lon, humidity, temp) into an array, displaying it into the console
       const locationInput = data[0];
 
       console.log(locationInput)
     });
+  
   
 }
 
